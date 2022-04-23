8f8afe687b1f0a8ab3f2913334058e8b7bbbce23
ellensofia <ellensofiadahlgren@outlook.com>
2022-04-24 00:02:04 +0200

    added links

--- header.php
+++ header.php
@@ -11,24 +11,23 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"> 
-    <script src="menu.js"></script>
     <?php wp_head(); ?>
 </head>
 <body>
     <div class="container">
         <!-- Header -->
-        <header class="header">
+        <header class="header" id="site-header">
             <div class="header__inner">
                 <!-- Logo -->
-                <a class="branding" href="#">
+                <a class="branding" href="http://lisadahlgren.local/">
                     <img src="<?php echo get_template_directory_uri()?>/Assets/Icons/LD.svg" alt="logotype">
                 </a>
                 <!-- Menu -->
                 <nav class="nav">
                     <div class="menu">
                         <ul class="menu__list">
-                            <li class="menu__item"><a class="menu__link transition" href="portfolio.php">Portfolio</a></li>
-                            <li class="menu__item"><a class="menu__link transition" href="#about">Om mig</a></li>
+                            <li class="menu__item"><a class="menu__link transition" href="http://lisadahlgren.local/portfolio/">Portfolio</a></li>
+                            <li class="menu__item"><a class="menu__link transition" href="http://lisadahlgren.local/#about">Om mig</a></li>
                             <li class="menu__item"><a class="menu__link transition"  href="#contact">Kontakt</a></li>
                         </ul>
                     </div>
@@ -48,4 +47,5 @@
                     </span>
             </div>
         </header>
+        
         <main class="content">
